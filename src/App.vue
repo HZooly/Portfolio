d7c1f7e5dc9a73cf4fa6bbc8e369bffb329608c2
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2019-01-05 02:43:32 +0100

    Bootstrap i18n system

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,31 @@
     <div id="wrapper" class="flex flex-col h-screen">
       <div id="content" class="flex-1 container mx-auto py-4 text-center antialiased leading-normal">
         <h1 class="text-grey-darker">Hugo Torzuoli</h1>
-        <p class="py-2">Hey ! I am Hugo, a 23 years old developer from France 🇫🇷 working with <a class="no-underline text-yellow-dark font-bold" href="https://www.versusmind.eu/" target="_blank">Versusmind 🐥</a></p>
+        <p class="py-2">{{$t("who-am-i")}} <a class="no-underline text-yellow-dark font-bold" href="https://www.versusmind.eu/" target="_blank">Versusmind 🐥</a></p>
         <img class="w-48 h-48 my-2" src="./assets/img/profile.png" alt="Profile Avatar">
         <p>
-          I love modern web technologies and work with
+          {{$t("technologies")}}
           <span class="text-grey-darkest font-bold">HTML/CSS</span>,
           <span class="text-grey-darkest font-bold">JavaScript</span>,
-          <span class="text-grey-darkest font-bold">Angular</span> (from AngularJS to Angular 7),
+          <span class="text-grey-darkest font-bold">Angular</span> ({{$t("from")}} AngularJS {{$t("to")}} Angular 7),
           <span class="text-grey-darkest font-bold">VueJS</span>,
           <span class="text-grey-darkest font-bold">Node</span>.
         </p>
         <p class="py-2">
-          In my free time I do some project on
+          {{$t("freetime")}}
           <a class="no-underline text-blue-dark" href="https://github.com/HZooly" target="_blank">GitHub</a>,
-          help people on <a class="no-underline text-blue-dark" href="https://stackoverflow.com/users/4781975/zooly" target="_blank">StackOverflow</a>
-          and post some words on <a class="no-underline text-blue-dark" href="https://dev.to/zooly">Dev Community</a>.
+          {{$t("help")}} <a class="no-underline text-blue-dark" href="https://stackoverflow.com/users/4781975/zooly" target="_blank">StackOverflow</a>
+          {{$t("post-words")}} <a class="no-underline text-blue-dark" href="https://dev.to/zooly">Dev Community</a>.
         </p>
         <p class="py-2">
-          You can find me on <a class="no-underline text-blue-dark" href="https://twitter.com/__Zooly" target="_blank">Twitter</a>,
-          <a class="no-underline text-blue-dark" href="https://www.linkedin.com/in/hugotorzuoli/" target="_blank">LinkedIn</a> and
+          {{$t("find-me")}} <a class="no-underline text-blue-dark" href="https://twitter.com/__Zooly" target="_blank">Twitter</a>,
+          <a class="no-underline text-blue-dark" href="https://www.linkedin.com/in/hugotorzuoli/" target="_blank">LinkedIn</a> {{$t("and")}}
           <a class="no-underline text-blue-dark" href="https://www.instagram.com/__zooly/" target="_blank">Instagram</a>.
         </p>
       </div>
       <footer class="w-full text-center py-1 border-t text-sm">
-        This website has been made with <a class="no-underline text-blue-dark" href="https://vuejs.org/" target="_blank">VueJS</a> and
-        <a class="no-underline text-blue-dark" href="https://tailwindcss.com/" target="_blank">TailwindCSS</a> and deployed with
+        {{$t("made-with")}} <a class="no-underline text-blue-dark" href="https://vuejs.org/" target="_blank">VueJS</a> {{$t("and")}}
+        <a class="no-underline text-blue-dark" href="https://tailwindcss.com/" target="_blank">TailwindCSS</a> {{$t("deployed")}}
         <a class="no-underline text-blue-dark" href="https://surge.sh/" target="_blank">Surge</a>
       </footer>
     </div>
